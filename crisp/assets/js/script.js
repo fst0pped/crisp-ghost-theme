3b1da0d0b488eee81993e01bb36af20af0ad3a5a
fst0pped <andy_dragonattack@hotmail.com>
2014-11-24 13:43:06 +0000

    Excerpt from first post displayed by default
    Also cleaned up slide behaviour, all with jQuery. Hopefully it doesn't
    break too much on older browsers.

--- crisp/assets/js/script.js
+++ crisp/assets/js/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
+	//hide all excerpts except for the first post on the index
 	$(".post-excerpt").hide();
+	$('.post:first-child').children('.post-excerpt').show();
+	$('.post:first-child .post-link').css('font-size','2em');
+
+	//control slide animation the reveal excerpts for all posts after the first
 	$(".post").mouseenter(function(){
-		$(this).children(".post-excerpt").slideDown("fast");
+		$(this).children(".post-excerpt").stop(true,true).slideDown(200);
 	});
-	$(".post").mouseleave(function(){
-		$(this).children(".post-excerpt").slideUp("slow");
+	$(".post:gt(0)").mouseleave(function(){
+		$(this).children(".post-excerpt").stop(true,true).slideUp(300);
 	});
 });
